c8e71fb707b8eb0b8c611f672a3313823fc90775
Mevlut Arslan <mevlutarslan@Mevluts-MacBook-Pro.local>
2020-06-23 00:28:03 +0200

    reworked login and signup mechanics

--- Models.py
+++ Models.py
@@ -1,20 +1,22 @@
-from main import db
+from . import db 
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
-    userHandle = db.Column(db.String(15), unique=True, primary_key=True)
-    username = db.Column(db.String(30), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_handle = db.Column(db.String(15), unique=True)
+    display_name = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     tweets = db.relationship('Tweet', backref="owner")
-    # profilePictureUrl = Column(String(200), nullable=True)
+    # profile_picture_url = Column(String(200), nullable=True)
 
 
 class Tweet(db.Model):
     __tablename__ = 'tweet'
 
     id = db.Column(db.Integer, unique=True, primary_key=True)
-    tweetOwnerHandle = db.Column(db.String(15), db.ForeignKey('user.userHandle'))
+    tweet_owner_handle = db.Column(db.String(15), db.ForeignKey('user.id'))
     text = db.Column(db.String(140), nullable=False)
     likes = db.Column(db.Integer)
--- main.py
+++ main.py
@@ -1,80 +1,15 @@
-from flask import Flask, url_for, render_template, request, redirect, session
-# Response,redirect
-import os
-import uuid
-from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, render_template, url_for
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
 
 
-app = Flask(__name__)
-SESSION_TYPE = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///relationships.db'
-app.config['SECRET_KEY'] = uuid.uuid4().hex
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-# session["logInStatus"] = False
-# session["loggedUser"] = None
-
-# Apparently I need this to run db.create_all() without problems 😅
-# from Models import User, Tweet
-from Models import User
-
-
-@app.route('/')
+@main.route('/')
 def index():
-    if session['loggedUserHandle'] != None:
-        print(session["loggedUserHandle"])
-        return render_template("index.html", logStatus=session["logInStatus"], loggedAccount=session["loggedUserHandle"])
-    return render_template("index.html", logStatus=session["logInStatus"])
+    return render_template("index.html")
 
 
-@app.route('/LogIn', methods=["GET", "POST"])
-def logIn():
-    error = ""
-    if request.method == "POST":
-        userHandle = request.form["userHandle"]
-        password = request.form["password"]
-
-        userInDatabase = User.query.filter_by(userHandle=userHandle).first()
-        print(userInDatabase)
-        if(userInDatabase != None):
-            if(userInDatabase.password == password):
-                session["logInStatus"] = True
-                session["loggedUserHandle"] = userInDatabase.userHandle
-                return redirect("/")
-            else:
-                error = "Wrong Password!"
-        else:
-            error = "Wrong Handle"
-    return render_template("logIn.html", thrownError=error)
-
-
-@app.route('/CreateAccount', methods=["GET", "POST"])
-def createAccount():
-    if request.method == "POST":
-        userHandle = request.form["userHandle"]
-        userPassword = request.form["password"]
-        userDisplayName = request.form["displayName"]
-        db.session.add(User(userHandle=userHandle, username=userDisplayName,
-                            password=userPassword))
-        db.session.commit()
-    return redirect("/LogIn")
-
-
-# snippet to fix static files not updating
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path, endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@main.route('/explore')
+@login_required
+def explore():
+    return render_template('explore.html', user=current_user)
